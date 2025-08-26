144750a4c227c531069d6721fcd12425e1fc5df4
Kimiya Morozova <kimiya@mail.aura-sec.net>
2025-08-26 11:03:33 +0200

    .env hinzugefügt

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import signal
 from flask import Flask, render_template
 from routes import register_routes
 from state import IFACE, POLL_INTERVAL_SEC, HISTORY_MINUTES, HOST, PORT
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 register_routes(app)
