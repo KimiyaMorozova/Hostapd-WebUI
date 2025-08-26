3acbad0603a13932ba3c49e74702279f6e01d062
Kimiya Morozova <kimiya@mail.aura-sec.net>
2025-08-26 15:01:14 +0200

    dwa

--- Modules/hostapd.py
+++ Modules/hostapd.py
@@ -18,12 +18,12 @@ def hostapd_status():
         data = out.decode("utf-8").strip().split("\n")
         data = {k: v for k, v in (line.split("=", 1) for line in data if "=" in line)} # each value gets a assinge
         data = {k.strip(): v.strip() for k, v in data.items()}  # remove whitespace
-        
+
 
         return {
             "active": "Yes",
             "interface": IFACE,
-            "ssid": data.get("ssid", "—"),
+            "ssid": data.get("ssid[0]", "—"),
             "channel": data.get("channel", "—"),
             "frequency": data.get("freq", "—")
         }
